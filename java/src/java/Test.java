0c5f0eba9f480e9978bcf4f43b23183249649071
seyoung <sayyoung878gmail.com>
2023-10-20 10:40:45 +0900

    재시도

--- java/src/java/Test.java
+++ java/src/java/Test.java
@@ -4,11 +4,7 @@ public class Test {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-<<<<<<< HEAD
 		System.out.println("안녕하세요?");
-=======
-		System.out.println("안녕하세요");
->>>>>>> branch 'main' of https://github.com/Jangseyoung0/java.git
 	}
 
 }
